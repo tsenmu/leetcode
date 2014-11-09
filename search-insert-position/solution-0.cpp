5b7e3eefe725f808a022ff19f9ec330a83ae6b30
Jingjie Zheng <i@tsenmu.com>
2014-11-09 17:53:01 -0500

    added solution to search insert position

--- search-insert-position/solution-0.cpp
+++ search-insert-position/solution-0.cpp
@@ -1,2 +1,19 @@
 class Solution {
-    pub
+public:
+    int searchInsert(int A[], int n, int target) {
+      int l = 0;
+      int r = n - 1;
+      int m;
+      while (l <= r) {
+        m = (l + r) >> 1;
+        if (target < A[m]) {
+          r = m - 1;
+        } else if (target > A[m]) {
+          l = m + 1;
+        } else {
+          return m;
+        }
+      }
+      return A[m] > target ? m : m + 1; 
+    }
+};
