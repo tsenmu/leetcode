be1f463030e2d5f067c44c94f9c60033b7bfe412
Jingjie Zheng <i@tsenmu.com>
2015-02-10 23:49:29 -0500

    added answer to pascals-triangle

---
+++ pascals-triangle/solution-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int> > generate(int numRows) {
+      vector<vector<int> > ret;
+      for (int i = 0; i < numRows; ++i) {
+        vector<int> row;
+        row.push_back(1);
+        for (int j = 1; j < i; ++j) {
+          if (j == i - 1) {
+            row.push_back(1);
+          } else {
+            row.push_back(ret[i - 1][j - 1] + ret[i - 1][j]);
+          }
+        }
+        ret.push_back(row);
+      }        
+      return ret;
+    }
+};
