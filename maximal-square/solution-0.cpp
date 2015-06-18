236ab5a70b1028fcb7f4dcc633071d73e365dfc1
Jingjie Zheng <i@tsenmu.com>
2015-06-17 20:16:20 -0400

    added solution to maximal square

--- maximal-square/solution-0.cpp
+++ maximal-square/solution-0.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
   int maximalSquare(vector<vector<char> >& matrix) {
-    vector<vector<pair<int, int> > > dp;
+    vector<vector<int> > dp;
+    int max_val = 0;
     for (int i = 0; i < matrix.size(); ++i) {
-      vector<char>& row = matrix[i];
-      vector<pair<int, int> > dpRow;
-      for (int j = 0; j < row.size(); ++j) {
-        char ch = matrix[i][j];
-        int r = 0;
-        int c = 0;
-        if (i == 0) {
-          r = (ch == '1' ? 1 : 0);
-        } else {
-
+      dp.push_back(vector<int>());
+      vector<int>& dp_row = dp[dp.size() - 1];
+      for (int j = 0; j < matrix[i].size(); ++j) {
+        int v = (int)(matrix[i][j] == '1');
+        if (i > 0 && j > 0) {
+          if (v) {
+            v = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+          } else {
+            v = 0;
+          }
         }
-        if (j == 0) {
-
-        } else {
-          
-        }
-        dpRow.push_back(make_pair(r, c));
+        max_val = max(v, max_val);
+        dp_row.push_back(v);
       }
     }
+    return max_val * max_val;
   }
 };
