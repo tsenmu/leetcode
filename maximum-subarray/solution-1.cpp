1b9390010a711f96a13da161d89f116ef5b09e64
Jingjie Zheng <i@tsenmu.com>
2015-07-12 12:07:56 -0400

    added another solution to maximum subarray

---
+++ maximum-subarray/solution-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  int calculateMaxSubArray(vector<int>& nums, int l, int r) {
+    if (l == r) {
+      return nums[l];
+    }
+    int m = (l + r) >> 1;
+    int leftMax = calculateMaxSubArray(nums, l, m);
+    int rightMax = calculateMaxSubArray(nums, m + 1, r);
+
+    int sum = nums[m];
+    int leftPartMax = sum;
+    for (int i = m - 1; i >= l; --i) {
+      sum += nums[i];
+      leftPartMax = max(sum, leftPartMax);
+    }
+    int sum = nums[m + 1];
+    int rightPartMax = sum;
+    for (int i = m + 2; i <= r; ++i) {
+      sum += nums[i];
+      rightPartMax = max(sum, rightPartMax);
+    }
+    return max(rightPartMax + leftPartMax, max(leftMax, rightMax));
+  }
+  int maxSubArray(vector<int>& nums) {
+    return calculateMaxSubArray(nums, 0, nums.size() - 1);
+  }
+};
