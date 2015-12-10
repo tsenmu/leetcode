d32a3b350e7b2a1c837858628842b8a4c126058e
Jingjie Zheng <i@tsenmu.com>
2015-12-09 23:30:06 -0500

    added solutions

--- remove-duplicates-from-sorted-array-ii/solution-0.cpp
+++ remove-duplicates-from-sorted-array-ii/solution-0.cpp
@@ -5,10 +5,17 @@ public:
             return 0;
         }
         int top = 1;
+        int counter = 1;
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] != nums[i - 1]) {
+                counter = 1;
                 nums[top++] = nums[i];
-            } 
+            } else {
+                counter++;
+                if (counter == 2) {
+                    nums[top++] = nums[i];
+                } 
+            }
         }
         return top;
     }
