5a98ee4f55a3be624e515574aa807305fe8bd0d5
Jingjie Zheng <i@tsenmu.com>
2015-12-10 21:50:46 -0500

    added solutions

--- simplify-path/solution-0.cpp
+++ simplify-path/solution-0.cpp
@@ -1,6 +1,45 @@
 class Solution {
 public:
-  string simplifyPath(string path) {
-          
-  }
+
+    void process_token(vector<string>& buf, string& token) {
+        if (token.length() != 0) {
+            if (token == ".") {
+                // ignore 
+            } else if (token == "..") {
+                if (buf.size() > 0)  {
+                    buf.pop_back();
+                }
+            } else {
+                buf.push_back(token);
+            }
+        }
+        token.clear();
+    }
+
+    string simplifyPath(string path) {
+
+        vector<string> buf;
+
+        string token;
+        for (int i = 0; i < path.length(); ++i) {
+            if (path[i] == '/') {
+                process_token(buf, token);
+            } else {
+                token += path[i];
+            }
+        }
+
+        process_token(buf, token);
+
+        string ans;
+        for (int i = 0; i < buf.size(); ++i) {
+            ans += "/" + buf[i];
+        }
+
+        if (ans.empty()) {
+            ans = "/" ;
+        }
+
+        return ans;
+    }
 };
