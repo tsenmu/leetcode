942e109be0cba14d7f21d0b8ad0c5d512ed431b9
Jingjie Zheng <i@tsenmu.com>
2016-01-18 22:07:06 -0500

    added solutions

--- gray-code/solution-0.cpp
+++ gray-code/solution-0.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> result((1 << n), 0);
+        const int total = 1 << n;
+        vector<int> result(total, 0);
 
-        while (n--) {
-            for (int i = (1 << n); i < ; ++i) {
-
+        int digit = 0;
+        for (int i = 1; i < total; ++i) {
+            result[i] = (1 << digit) | result[(1 << (digit + 1)) - i - 1];
+            if (i + 1 == (1 << (digit + 1))) {
+                digit++;
             }
         }
+
+        return result; 
     }
 };
