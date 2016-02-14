f332c672bbb428f8046eb419d9e518ef7c7029e2
Jingjie Zheng <i@tsenmu.com>
2016-02-13 19:24:51 -0500

    added solutions

--- wildcard-matching/solution-0.cpp
+++ wildcard-matching/solution-0.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        const int n = s.length();
-        const int m = p.length();
-        vector<vector<bool>> dp(n + 1, vector<int>(m + 1, false));
-        
+        int n = s.length();
+        int m = p.length();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
         dp[0][0] = true;
 
-        for (int i = 0; i < n; ++i) {
-            char ch = s[i];
-            for (int j = 0; j < m; ++j) {
-                char pattern = p[j];
-                dp[i + 1][j + 1] = dp[i][j];
-
-                for (int k = 0; k < j; ++k) {
-                    dp[i + 1][j + 1] =  
+        for (int i = 1; i <= m; ++i) {
+            char pattern = p[i - 1];
+            if (pattern == '*') {
+                for (int j = 0; j <= n; ++j) {
+                    if (dp[j][i - 1]) {
+                        for (int k = j; k <= n; ++k) {
+                            dp[k][i] = true; 
+                        }
+                    }
                 }
-
+            } else if (pattern == '?') {
+                for (int j = 0; j < n; ++j) {
+                    if (dp[j][i - 1]) {
+                        dp[j + 1][i]  = true;
+                    }
+                }
+            } else {
+                for (int j = 0; j < n; ++j) {
+                    if (dp[j][i - 1] && s[j] == pattern) {
+                        dp[j + 1][i] = true;
+                    }
+                }
             }
+            
+            // for (int ii = 0; ii <= n; ++ii) {
+            //     for (int jj = 0; jj <= m; ++jj) {
+            //         printf("%d ", dp[ii][jj] ? 1 : 0);
+            //     }
+            //     printf("\n");
+            // }
+            // printf("\n");
         }
 
         return dp[n][m];
