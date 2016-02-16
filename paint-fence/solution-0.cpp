4b1d096b4b79c640608b0e774b0c123cc0672ec4
Jingjie Zheng <i@tsenmu.com>
2016-02-16 17:59:21 -0500

    added solutions

--- paint-fence/solution-0.cpp
+++ paint-fence/solution-0.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int numWays(int n, int k) {
-               
+        if (n == 0) {
+            return 0;
+        }
+        
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        
+        dp[0][0] = k;
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = max(0, dp[i - 1][0] * (k - 1)) + max(0, dp[i - 1][1] * (k - 1));
+            dp[i][1] = dp[i - 1][0];
+        }
+        
+        return dp[n - 1][0] + dp[n - 1][1];
     }
 };
