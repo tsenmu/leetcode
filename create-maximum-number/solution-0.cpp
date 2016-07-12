48af43440795727aeaa066de1ff6d80b3af9a63d
Jingjie Zheng <i@tsenmu.com>
2016-07-11 23:25:51 -0700

    added solutions

---
+++ create-maximum-number/solution-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+        int p = 0;
+        int q = 0;
+        int m = nums1.size();
+        int n = nums2.size();
+
+        m - p 
+    }
+};
