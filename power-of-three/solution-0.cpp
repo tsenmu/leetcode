b40d09231fbc2aa73d0b7da495fe771be0f274fe
Jingjie Zheng <i@tsenmu.com>
2016-07-30 22:27:37 -0700

    added solutions

--- power-of-three/solution-0.cpp
+++ power-of-three/solution-0.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        int maxPowerOfThree = (int)pow(3, (int)(log(INT_MAX)/log(3)));
+        int maxPowerOfThree = (int)pow(3, (int)(log(INT_MAX) / log(3)));
         return n > 0 && maxPowerOfThree % n == 0;
     }
 };
