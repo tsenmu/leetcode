b4e4e84216dbfa468ee40ad8d50486ebf7855366
Jingjie Zheng <i@tsenmu.com>
2016-07-30 21:30:54 -0700

    added solutions

--- paint-fence/solution-0.cpp
+++ paint-fence/solution-0.cpp
@@ -4,15 +4,15 @@ public:
         if (n == 0) {
             return 0;
         }
-        
-        vector<vector<int>> dp(n, vector<int>(2, 0));
-        
-        dp[0][0] = k;
+
+        int dp[n][2] = {0};
+        dp[0][0] = 0;
+        dp[0][1] = k;
         for (int i = 1; i < n; ++i) {
-            dp[i][0] = max(0, dp[i - 1][0] * (k - 1)) + max(0, dp[i - 1][1] * (k - 1));
-            dp[i][1] = dp[i - 1][0];
+            dp[i][0] = dp[i - 1][1];
+            dp[i][1] = (dp[i - 1][1] + dp[i - 1][0]) * (k - 1);
         }
-        
+
         return dp[n - 1][0] + dp[n - 1][1];
     }
 };
