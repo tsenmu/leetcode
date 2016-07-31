1818b926d8218dd72e823a8ba3f35c6d8138c04a
Jingjie Zheng <i@tsenmu.com>
2016-07-30 21:03:13 -0700

    added solutions

--- unique-word-abbreviation/solution-0.cpp
+++ unique-word-abbreviation/solution-0.cpp
@@ -1,33 +1,25 @@
 class ValidWordAbbr {
 protected:
-    unordered_map<string, int> m_count;
-    unordered_set<string> m_dict;
-    string calculateAbbreviation(const string& str) {
-        const int n = str.length();
-        if (n <= 2) {
-            return str; 
-        } 
-        return str[0] + to_string(n - 2) + str[n - 1];
+    unordered_map<string> m_set;
+    string calculateAbbreviation(const string& word) {
+        if (word.length() <= 2) {
+            return word;
+        } else {
+            ostringstream oss;
+            oss << word[0] << word.length() - 2 << word[word.length() - 1];
+            return oss.str();
+        }
     }
 public:
     ValidWordAbbr(vector<string> &dictionary) {
-        const int n = dictionary.size();
-        for (int i = 0; i < n; ++i) {
-            m_dict.insert(dictionary[i]);
-            
-        }
-        
-        for (auto it = m_dict.begin(); it != m_dict.end(); ++it) {
-            m_count[calculateAbbreviation(*it)]++;
+        for (int i = 0; i < dictionary.size(); ++i) {
+           const stirng& word = dictionary[i];
+            m_set.insert(calculateAbbreviation(word));
         }
     }
 
     bool isUnique(string word) {
-        string abbr = calculateAbbreviation(word);
-        if (m_dict.count(word) == 1) {
-            return m_count[abbr] == 1;
-        }
-        return m_count[abbr] == 0;
+        return m_set.count(calculateAbbreviation(word)) == 0; 
     }
 };
 
