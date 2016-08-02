65026ae6ff7a009179a403945f9f1de823537b01
Jingjie Zheng <i@tsenmu.com>
2016-08-01 18:10:45 -0700

    added solutions

--- wiggle-sort/solution-0.cpp
+++ wiggle-sort/solution-0.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        const int n = nums.size();
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < nums.size(); ++i) {
             if (i & 1) {
                 if (nums[i - 1] > nums[i]) {
                     swap(nums[i - 1], nums[i]);
