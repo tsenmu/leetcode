352055173f087f83846e90afcf809a04072e9992
Jingjie Zheng <i@tsenmu.com>
2016-08-03 23:31:37 -0700

    added solutions

--- perfect-squares/solution-0.cpp
+++ perfect-squares/solution-0.cpp
@@ -1,9 +1,39 @@
 class Solution {
+protected:
+    unordered_map<int, int> m_sol;
+    vector<int> m_perfectSquares;
 public:
-  int numSquares(int n) {
-    
-    for (int i = 0; i < n; ++i) {
+    int numSquares(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        if (m_sol.count(n) != 0) {
+            return m_sol[n];
+        }
 
+        if (m_perfectSquares.size() == 0) {
+            int number = 1;
+            while (number * number <= n) {
+                m_perfectSquares.push_back(number * number);
+                ++number;
+            }
+        }
+
+        for (int i = m_perfectSquares.size() - 1; i >= 0; --i) {
+            int perfectSquare = m_perfectSquares[i];
+            if (perfectSquare == n)  {
+                return m_sol[n] = 1;
+            } else if (perfectSquare < n) {
+                int proposedSol = n / perfectSquare + numSquares(n % perfectSquare);
+                if (m_sol.count(n) == 0) {
+                    m_sol[n] = proposedSol;
+                } else {
+                    m_sol[n] = min(m_sol[n], proposedSol);
+                }
+            }
+        }
+
+        return m_sol[n];
     }
-  }
 };
