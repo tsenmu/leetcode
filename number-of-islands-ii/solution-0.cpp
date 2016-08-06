59b74a515f947af2d67ae0c18d85487c7d89dca9
Jingjie Zheng <i@tsenmu.com>
2016-08-06 13:48:58 -0700

    added solutions

--- number-of-islands-ii/solution-0.cpp
+++ number-of-islands-ii/solution-0.cpp
@@ -1,98 +1,79 @@
 class Solution {
 protected:
-    vector<int> m_diset;
-    // will return target's parent
-    int diset_find(int target) {
-        if (m_diset[target] == target) {
-            return target;
+    int diset_find(int x, vector<int>& diset) {
+        if (diset[x] == x) {
+            return x; 
         }
-        return m_diset[target] = diset_find(m_diset[target]);
+        return diset[x] = diset_find(diset[x], diset);
     }
 
-    // will union two sets.
-    bool diset_union(int x, int y) {
-        int xp = diset_find(x);
-        int yp = diset_find(y);
-        if (xp == yp) {
+    bool diset_union(int x, int y, vector<int>& diset) {
+        int xr = diset_find(x, diset);
+        int yr = diset_find(y, diset);
+
+        if (xr == yr) {
             return false;
-        } 
-        m_diset[xp] = yp;
+        }
+        diset[xr] = yr;
         return true;
     }
 
-    // return the new set numbering
-    int diset_add() {
-        m_diset.push_back(m_diset.size());
-        return m_diset.size() - 1;
+    int diset_insert(vector<int>& diset) {
+        int ne = diset.size();
+        diset.push_back(ne);
+        return ne;
     }
 
-    bool is_valid(int r, int c, int m, int n) {
-        return r >= 0 && r < m && c >= 0 && c < n; 
+
+    bool isValidCoordinate(int r, int c, int row, int col) {
+        return r >= 0 && c >= 0 && r < row && c < col; 
     }
 public:
     vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
-        m_diset.clear();
-        m_diset.push_back(0);
-        vector<vector<int>> matrix(m, vector<int>(n, 0));
 
-        int dx[] = {0, 0, -1, 1};
-        int dy[] = {-1, 1, 0, 0};
+        vector<int> diset;
+        diset_insert(diset);
 
-        vector<int> ans;
-        int islands = 0;
-
+        static int dr[4] = {-1, 1, 0, 0};
+        static int dc[4] = {0, 0, -1, 1};
+        vector<vector<int>> grid(m, vector<int>(n, 0));
+        int ans = 0;
+        vector<int> ret;
         for (int i = 0; i < positions.size(); ++i) {
-            int r = positions[i].first;
+            int r = positions[i].first; 
             int c = positions[i].second;
 
-            vector<int> neighbour_colours;
-            // directions
+            set<int> islands;
             for (int j = 0; j < 4; ++j) {
-                int nr = r + dx[j];
-                int nc = c + dy[j];
-                if (is_valid(nr, nc, m, n) && matrix[nr][nc] != 0) {
-                    neighbour_colours.push_back(matrix[nr][nc]);
-                }
-            }
-
-            if (neighbour_colours.size() == 0) {
-                matrix[r][c] = diset_add();
-                islands++;
-            } else if (neighbour_colours.size() == 1) {
-                matrix[r][c] = neighbour_colours[0];
-            } else {
-                for (int ii = 0; ii < neighbour_colours.size(); ++ii) {
-                    for (int jj = ii + 1; jj < neighbour_colours.size(); ++jj) {
-                        if (diset_union(neighbour_colours[ii], neighbour_colours[jj])) {
-                            islands--;
-                        } 
+                int nr = r + dr[j];
+                int nc = c + dc[j];
+                if (isValidCoordinate(nr, nc, m, n))  {
+                    if (grid[nr][nc]) {
+                        islands.insert(grid[nr][nc]);
                     }
                 }
-                matrix[r][c] = neighbour_colours[0];
             }
 
-            ans.push_back(islands);
+            vector<int> islandsVec(islands.begin(), islands.end());
+            if (islandsVec.size() == 0) {
+                grid[r][c] = diset_insert(diset);
+                ans++;
+            } else if (islandsVec.size() == 1) {
+                grid[r][c] = islandsVec[0];
+            } else {
+                int last = islandsVec[0];
+                grid[r][c] = last;
+                for (int j = 1; j < islandsVec.size(); ++j) {
+                    if (diset_union(islandsVec[j], islandsVec[j - 1], diset)) {
+                        ans--;
+                    }
+                }
+            }
+            ret.push_back(ans);
         }
-
-        return ans;
+        return ret;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
