c589cde2cc16654caad9b43a965e024858e9def9
Jingjie Zheng <i@tsenmu.com>
2016-08-06 23:58:05 -0700

    added solutions

--- plus-one-linked-list/solution-0.cpp
+++ plus-one-linked-list/solution-0.cpp
@@ -9,6 +9,36 @@
 class Solution {
 public:
     ListNode* plusOne(ListNode* head) {
-                 
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* node = dummy;
+        
+        ListNode* affected = dummy;
+        while (node->next) {
+            node = node->next;
+            if (node->val != 9) {
+                affected = node;
+            }
+        }
+
+        if (node->val == 9) {
+            node->val = 0;
+        }
+
+        node = affected; 
+        node->val += 1;
+
+        while (node->next) {
+            node = node->next;
+            node->val = 0;
+        }
+
+        if (dummy->val == 0) {
+            delete dummy;
+            return head;
+        }
+
+        return dummy;
     }
 };
