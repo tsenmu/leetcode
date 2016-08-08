ceb4a5b3c0c8ab1815d2ad02a0d1671c83f83490
Jingjie Zheng <i@tsenmu.com>
2016-08-07 22:59:19 -0700

    added solutions

--- binary-tree-longest-consecutive-sequence/solution-0.cpp
+++ binary-tree-longest-consecutive-sequence/solution-0.cpp
@@ -8,32 +8,33 @@
  * };
  */
 class Solution {
-protected:
-    void dfs(TreeNode* root, int cur, int& ans) {
-        ans = max(ans, cur);
+public:
+    int dfs(TreeNode* root, int length, int& ans) {
+        ans = max(ans, length);
+
         if (root->left) {
-            if (root->left->val - root->val == 1) {
-                dfs(root->left, cur + 1, ans);
+            if (root->left->val == root->val + 1) {
+                dfs(root->left, length + 1, ans);
             } else {
                 dfs(root->left, 1, ans);
             }
         }
 
         if (root->right) {
-            if (root->right->val - root->val == 1) {
-                dfs(root->right, cur + 1, ans);
+            if (root->right->val == root->val + 1) {
+                dfs(root->right, length + 1, ans);
             } else {
                 dfs(root->right, 1, ans);
             }
         }
     }
-public:
+
     int longestConsecutive(TreeNode* root) {
-        if (root == NULL)  {
+        if (root == NULL) {
             return 0;
         }
+        ans = 0;
 
-        int ans = 1;
         dfs(root, 1, ans);
         return ans;
     }
