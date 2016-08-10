6b50699570e791fe1b656218203da0b5e26576e2
Jingjie Zheng <i@tsenmu.com>
2016-08-09 21:16:54 -0700

    added solutions

--- zigzag-iterator/solution-0.cpp
+++ zigzag-iterator/solution-0.cpp
@@ -1,86 +1,41 @@
 class ZigzagIterator {
 protected:
-    vector<vector<int>> m_vectors; 
-    int m_maxVectorLength;
-    int m_currentIndex;
-    int m_currentVector;
-    void seekNextAvailablePosition() {
-        if (m_currentIndex == -1 || m_currentVector == -1) {
-            return;
-        }
-
-        // initialize next possible position
-        m_currentVector++;
-        if (m_currentVector == m_vectors.size()) {
-            m_currentVector = 0;
-            m_currentIndex++;
-        }
-
-        // validate next possible position and iterate subsequent 
-        // possible positions if the current one is not valid
-        bool found = false;
-        while (m_currentIndex < m_maxVectorLength) {
-            while (m_currentVector < m_vectors.size()) {
-                if (m_currentIndex < m_vectors[m_currentVector].size()) {
-                    found = true;
-                    break;
-                }
-                m_currentVector++;
-            }
-            if (found) {
-                break;
-            }
-            m_currentVector = 0;
-            m_currentIndex++;
-        }
-
-        if (m_currentIndex == m_maxVectorLength) {
-            m_currentVector = -1;
-            m_currentIndex = -1;
-        }
-    }
-
-    void findMaxVectorLength() {
-        m_maxVectorLength = 0;
-        for (int i = 0; i < m_vectors.size(); ++i) {
-            m_maxVectorLength = max(m_maxVectorLength, (int) m_vectors[i].size());
-        }
-    }
-
-    void findFirstValidPosition() {
-        m_currentIndex = 0;
-        m_currentVector = 0;
-        for (int i = 0; i < m_vectors.size(); ++i) {
-            if (m_vectors[i].size() > 0) {
-                break;
-            } 
-            m_currentVector++;
-        }
-
-        if (m_currentVector == m_vectors.size()) {
-            m_currentIndex = -1; 
-            m_currentVector = -1;
-        } 
-    }
+    list<vector<int>> m_vectors;
+    list<vector<int>>::iterator it;
+    int column;
 
 public:
     ZigzagIterator(vector<int>& v1, vector<int>& v2) {
-        m_vectors.clear();
-        m_vectors.push_back(v1);
-        m_vectors.push_back(v2);
-        findFirstValidPosition();
-        findMaxVectorLength();
+        if (!v1.empty()) {
+            m_vectors.push_back(v1);
+        }
+        if (!v2.empty()) {
+            m_vectors.push_back(v2);
+        }
+        it = m_vectors.begin();
+        column = 0;
     }
 
     int next() {
-        assert(m_currentVector != -1 && m_currentVector != -1); 
-        int ret = m_vectors[m_currentVector][m_currentIndex];
-        seekNextAvailablePosition();
+        int ret = -1;
+        vector<int>& vec = *it;
+        ret = vec[column];
+        if (column == vec.size() - 1) {
+            it = m_vectors.erase(it); 
+        } else {
+            ++it;
+        }
+        if (it == m_vectors.end()) {
+            if (!m_vectors.empty()) {
+                it = m_vectors.begin();
+                column++;
+            }
+        }
         return ret;
     }
 
     bool hasNext() {
-        return m_currentIndex != -1 && m_currentVector != -1;
+        return !m_vectors.empty();
     }
 };
 
